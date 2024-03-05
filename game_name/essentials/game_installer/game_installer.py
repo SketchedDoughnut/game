7138c64d475c7171acc87a5a711a66e2f5fba46f
SketchedDoughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-03-04 22:11:57 -0800

    x

--- game_name/essentials/game_installer/game_installer.py
+++ game_name/essentials/game_installer/game_installer.py
@@ -132,8 +132,9 @@ class Install:
                 # downloading
                 #downloader.download(self.install_path, 'game_name', True)
                 downloader.download(self.install_path)
-                print(f'{self.install_path}/game_name/essentials/run_game/content_url.txt')
-                f = open(f'{self.install_path}/game_name/essentials/run_game/content_url.txt', 'w')
+                url_path = f'{self.install_path}/game_name/essentials/run_game/content_url.txt'
+                print(url_path)
+                f = open(url_path, 'w')
                 f.write(f'{self.install_path}/game_data/game_content/main.py')
                 f.close()
             
--- game_name/essentials/run_game/run_game.py
+++ game_name/essentials/run_game/run_game.py
@@ -1,7 +1,9 @@
 import os
 
 #run_path = 'python ../game_data/game_content/main.py'
-f = open()
+f = open('content_url.txt', 'r')
+run_path = f.read()
+f.close()
 
 print(f'Running game: {run_path}')
 os.system(run_path)
