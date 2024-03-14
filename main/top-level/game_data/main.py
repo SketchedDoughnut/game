b29069a8afc0a304b1cd17dc65f8a295c2e3997a
SketchedDoughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-03-13 22:25:08 -0700

    almost working flappy bird, fix wall gen

--- main/top-level/game_data/main.py
+++ main/top-level/game_data/main.py
@@ -1,94 +1,160 @@
+# imports
 import pygame
 from pygame.locals import *
+import random
 
+# init
 pygame.init()
 
-# https://www.techwithtim.net/tutorials/game-development-with-python/pygame-tutorial/pygame-tutorial-movement
-#screen_width = 500
-#screen_height = 500
-
+# dimensions of screen
 w, h = pygame.display.Info().current_w, pygame.display.Info().current_h
 
+# window settings
 window = pygame.display.set_mode((w, h))
 pygame.display.set_caption("thing!")
 
-x = 50
-y = 50
-width = 40
-height = 40
-vel = 2.5
+# cube data
+class Cube:
+    def __init__(self):
 
-def borders(axis, vel, mode):
-    if False:
-        pass
+        # position
+        self.x = 500
+        self.y = h / 2
 
-    else:
-        if mode == 'a':
-            axis += vel
+        # sizing
+        self.width = 40
+        self.height = 40
 
-        elif mode == 's':
-            axis -= vel
+        # motion
+        self.yv = 0
+        self.moving = False
 
-    print(axis)
-    return axis
+        # environment
+        self.grav = 0.5
+        self.jump = -7.5
 
-color = (255, 255, 255)
+    # pick color
+    def pick_color(self):
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) 
 
-run = True
+    # generate gravity on cube
+    def gravity(self):
+        self.yv += self.grav
+    
+    # update cubes location
+    def update_location(self):
+        self.y += self.yv 
 
-while run:
-    #pygame.time.delay(100)
+    # make cube jump
+    def jumping(self):
+        self.yv = self.jump
+    
+cube = Cube()
 
+class Walls:
+    def __init__(self):
+
+        # position
+        self.x = w
+
+        # sizing
+        self.width = 100
+        self.height = 550
+
+    def b_vertical(self):
+        self.y = h
+        self.y -= h / random.randint(1, 10)
+
+    def t_vertical(self):
+        self.y = h
+        self.y = 0.025 * (h / 10)
+
+    # movement
+    def move_wall(self):
+        self.x -= 5
+
+    # pick color
+    def pick_color(self):
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)) 
+
+wall_1 = Walls()
+wall_1.pick_color()
+wall_1.b_vertical()
+
+wall_2 = Walls()
+wall_2.pick_color()
+wall_2.t_vertical()
+
+def bounds():
+    global running
+    if cube.y >= h:
+        running = False
+
+    elif cube.y <= 0:
+        running = False
+
+# main loop
+running = True
+while running:
+
+    # timer for delay
+    pygame.time.delay(10)
+
+    # checking for events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
+            runing = False
 
+    # check all keys here
     keys = pygame.key.get_pressed()
-
     if keys[K_LCTRL]:
         if keys[K_w]:
-            run = False
+            running = False
 
-        if keys[K_e]:
-            window.fill((255, 0, 0))
-        if keys[K_r]:
-            window.fill((0, 255, 0))
-        if keys[K_f]:
-            window.fill((0, 0, 255))
-        if keys[K_SPACE]:
-            window.fill((255, 255, 255))
-    
-    if keys[K_x]:
-        window.fill((0, 0, 0))
+    if cube.moving == True:
+        cube.gravity()
 
-    if keys[K_a]:
-        #x -= vel
-        x = borders(x, vel, 's')
-
-    if keys[K_d]:
-        #x += vel
-        x = borders(x, vel, 'a')
-
-    if keys[K_w]:
-        #y -= vel
-        y  =borders(y, vel, 's')
-
-    if keys[K_s]:
-        #y += vel
-        y = borders(y, vel, 'a')
-
-    
-    # color changing
-    if keys[K_e]:
-        color = (255, 0, 0)
-    if keys[K_r]:
-        color = (0, 255, 0)
-    if keys[K_f]:
-        color = (0, 0, 255)
     if keys[K_SPACE]:
-        color = (255, 255, 255)
+        if cube.moving ==  False:
+            print('starting wall generation, wall movement, cube movement')
+        cube.moving = True
+        if cube.moving == True:
+            cube.jumping()
 
-    pygame.draw.rect(window, color, (x, y, width, height))   
-    pygame.display.update() 
+    # functions
+    if cube.moving == True:
+        cube.update_location()
+
+    cube.pick_color()
+
+    if wall_1.x < -5 and wall_2.x < -5:
+        print('generating new walls')
+        wall_1 = Walls()
+        wall_1.pick_color()
+        wall_1.b_vertical()
+        wall_2 = Walls()
+        wall_2.pick_color()
+        wall_2.t_vertical()
     
+    if cube.moving == True:
+        wall_1.move_wall()
+        wall_2.move_wall()
+
+    if cube.moving == True:
+        bounds() 
+
+    # make rect and update display
+    window.fill((0, 0, 0))
+    player = pygame.draw.rect(window, cube.color, (cube.x, cube.y, cube.width, cube.height))   
+    wall_rect_1 = pygame.draw.rect(window, wall_1.color, (wall_1.x, wall_1.y, wall_1.width, wall_1.height))     
+    wall_rect_2 = pygame.draw.rect(window, wall_2.color, (wall_2.x, wall_2.y, wall_2.width, wall_2.height))    
+
+    # https://www.youtube.com/watch?v=BHr9jxKithk 
+    if player.colliderect(wall_rect_1) or player.colliderect(wall_rect_2): 
+        print('wall impact')
+        running = False
+
+    pygame.display.update() 
+
+# quit if exit loop
 pygame.quit()
