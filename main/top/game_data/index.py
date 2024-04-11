a77da1378b14671a807ef07e32ad39645d57fe63
SketchedDoughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-04-11 08:10:39 -0700

    Update index.py

--- main/top/game_data/index.py
+++ main/top/game_data/index.py
@@ -7,3 +7,28 @@ import timeit
 import time
 import os
 
+pygame.init()
+
+WIDTH = 1920
+HEIGHT = 1080
+
+wDir = os.path.dirname(os.path.abspath(__file__))
+path_list = []
+path_list.append(os.path.join(wDir, 'src/flappy/flappy.py'))
+path_list.append(os.path.join(wDir, 'src/rhythm/rhythm.py'))
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+class Format:
+  def __init__(self):
+    self.x = None
+    self.y = None
+    self.width = None
+    self.height = None
+
+draw_var = Format()
+
+running = True
+while running: 
