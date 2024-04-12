facd7033cb23ed11ce2cdaed3dff853fa924142f
Sketched Doughnut <sketchedbusinessemail@gmail.com>
2024-04-11 23:47:09 -0700

    pretty please

--- main/top/game_data/index.py
+++ main/top/game_data/index.py
@@ -1,10 +1,10 @@
 # the coding gods have screwed me over and I need to redesign this program
 
 # imports
-import pygame
-from pygame.locals import *
 import pip
 pip.main(['install', 'pygame'])
+import pygame
+from pygame.locals import *
 import timeit
 import time
 import os
