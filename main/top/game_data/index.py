68547582be69eea168cad7589a3bf63e421b6730
SketchedDoughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-04-14 21:57:57 -0700

    x

--- main/top/game_data/index.py
+++ main/top/game_data/index.py
@@ -6,7 +6,7 @@ import os
 # vars
 wDir = os.path.dirname(os.path.abspath(__file__))
 path_list = []
-path_list.append([os.path.join(wDir, 'imports.py'), 'Import agent'])
+path_list.append([os.path.join(wDir, 'imports.py'), 'Imports agent'])
 path_list.append([os.path.join(wDir, 'update.py'), 'Update agent'])
 path_list.append([os.path.join(wDir, 'src/flappy/flappy.py'), 'Flappy bird'])
 path_list.append([os.path.join(wDir, 'src/rhythm/rhythm.py'), 'Rhythm'])
