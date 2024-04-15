6a3baf82c61918d38fe342b5ae70d8f475571592
SketchedDoughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-04-14 19:20:30 -0700

    reworking update launcher

--- main/top/game_data/update.py
+++ main/top/game_data/update.py
@@ -41,10 +41,13 @@ exit = False
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("thing!")
 
+
+
 def check_version():
     # globals
     global exit
     global text_msg
+    text_msg = 'Checking for updates...'
 
     # release destination, working directory, loading version
     dest = 'https://api.github.com/repos/SketchedDoughnut/development/releases/latest' # link format: https://api.github.com/repos/{owner}/{repo}/releases/latest
@@ -72,33 +75,14 @@ def check_version():
             print('No decrepancy: Exiting...')
             text_msg = 'No updates found.'
             time.sleep(2)
-            exit = True
+            #exit = True
 
+def buttons(yes_list, no_list):
+    yes_zone = pygame.Rect(yes_list[0], yes_list[1], yes_list[2], yes_list[3])
+    no_zone = pygame.Rect(no_list[0], no_list[1], no_list[2], no_list[3])
+    return [[GREEN, yes_zone], [RED, no_zone]]
 
-def cancel(mode=0):
-    global exit
-    global text_msg
-    global _cancel
-    global check
-    if mode == 0:
-        _cancel = True
-        text_msg = 'Are you sure you want to cancel?'
-    if mode == 1:
-        _cancel = False
-        text_msg = 'Exiting...'
-        check.join()
-        time.sleep(3)
-        exit = True
-    if mode == 2:
-        _cancel = False
-        try:
-            idk.join()
-        except:
-            print('no thread error')
-        check = threading.Thread(target=lambda:check_version(), daemon=True)
-        check.start()
-
-
+##########################################################################################
 
 print('----------------------------')
 print('NOTE: THIS UPDATE AGENT IS CURRENTLY DEPRECIATED. WHY? I AM LAZY.')
@@ -116,43 +100,41 @@ while not exit:
     mouse_pos = pygame.mouse.get_pos()
 
     # exits if
-    if keys[K_LCTRL]:
-            if keys[K_c]:
-                cancel(0)
-            elif keys[K_w]:
-                cancel(0)
-    if keys[K_ESCAPE]:
-        cancel(0)
+    # if keys[K_LCTRL]:
+    #         if keys[K_c]:
+    #             cancel(0)
+    #         elif keys[K_w]:
+    #             cancel(0)
+    # if keys[K_ESCAPE]:
+    #     cancel(0)
 
     window.fill(BLACK)
     font = pygame.font.Font('freesansbold.ttf', round(24))
     text = font.render(text_msg, True, WHITE, None) # text, some bool(?), text color, bg color
-    text_rect = text.get_rect(center=(WIDTH / 2, HEIGHT / 2))
+    text_rect = text.get_rect(center=(WIDTH / 2, HEIGHT / 4))
     window.blit(text, text_rect)
 
     if _cancel:
-        width = (500)
+        width = (250)
         height = (HEIGHT / 15)
         x = (WIDTH / 2) - (width / 2)
-        y = 3 * (HEIGHT / 5)
-        cancel_zone = pygame.draw.rect(window, GREEN, (x, y, width, height))
-        y = 4 * (HEIGHT / 5)
-        stop = pygame.draw.rect(window, RED, (x, y, width, height))
+        y = 2 * (HEIGHT / 4)
+        yes = [x, y, width, height] # green
+        y = 3 * (HEIGHT / 4)
+        no = [x, y, width, height] # red
+        objects = buttons(yes, no)
+        yes_zone = objects[0][1]
+        no_zone = objects[1][1]
+        pygame.draw.rect(window, objects[0][0], (yes_zone))
+        pygame.draw.rect(window, objects[1][0], (no_zone))
 
-        if cancel_zone.collidepoint(mouse_pos):
-            if pygame.mouse.get_pressed()[0]:
-                text_msg = 'Exiting...'
-                idk = threading.Thread(target=lambda:cancel(1), daemon=True)
-                idk.start()
-        if stop.collidepoint(mouse_pos):
-            if pygame.mouse.get_pressed()[0]:
-                cancel(2)
+        # if yes_zone.collidepoint(mouse_pos):
+        #     if pygame.mouse.get_pressed()[0]:
+        #         text_msg = 'Exiting...'
+        #         idk = threading.Thread(target=lambda:cancel(1), daemon=True)
+        #         idk.start()
+        # if no_zone.collidepoint(mouse_pos):
+        #     if pygame.mouse.get_pressed()[0]: 
+        #         cancel(2)
 
     pygame.display.update()
-
-'''
-  font = pygame.font.Font('freesansbold.ttf', round(36 * 1.5))
-  text = font.render(path_list[1][1], True, WHITE, None) # text, some bool(?), text color, bg color
-  text_rect = text1.get_rect(center=(WIDTH / 2, HEIGHT / 4))
-  window.blit(text1, text1_rect)
-'''
