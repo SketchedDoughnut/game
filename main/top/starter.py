e00cf84d1dcca07d69910c3866c4174b285ddd14
SketchedDoughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-04-15 15:52:34 +0000

    renaming

--- main/top/starter.py
+++ main/top/starter.py
@@ -2,8 +2,10 @@
 
 import os
 
-wDir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-path = os.path.join(wDir, 'top/container/index.py')
+#wDir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#path = os.path.join(wDir, 'top/container/index.py')
+wDir = os.path.dirname(os.path.abspath(__file__))
+path = os.path.join(wDir, 'container/index.py')
 print(path)
 
 print('Starter redirecting to index file...')
