66ad61daec220d2bfccf42c5e479e3d6eba2a91d
SketchedDoughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-04-15 18:04:51 +0000

    path changes

--- lazy_shortcut.py
+++ lazy_shortcut.py
@@ -2,8 +2,6 @@ import os
 wDir = os.path.dirname(os.path.abspath(__file__))
 
 # py path
-#path = os.path.join(wDir, 'main/top/starter.py')
+path = os.path.join(wDir, 'main/top/starter.py')
 
-# exe path
-path = os.path.join(wDir, 'main/top/starter.exe')
 os.system(f'python {path}')
