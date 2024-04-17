a58b5024a5fe9458ae29c44d70cef4dc5a8ba495
SketchedDoughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-04-17 00:17:21 -0700

    fix

--- main/setup/fiesta.py
+++ main/setup/fiesta.py
@@ -362,8 +362,12 @@ class Install:
                 else:
                     print('---------------')
                     print('Installer redirecting to starter file...')
-                    #self.top_wDir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-                    self.top_wDir = (os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+                    # FOR COMPILE
+                    self.top_wDir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+                    # FOR PYTHON
+                    #self.top_wDir = (os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
                     self.top_wDir = os.path.join(self.top_wDir, 'top')
                     os.system(f'python {self.top_wDir}/starter.py')
                     exit()
