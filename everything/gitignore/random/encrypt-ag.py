2ade6e1ef7f709d79eacf88967a56895112f9549
SketchedDoughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-04-22 15:53:35 +0000

    pre

--- everything/gitignore/random/encrypt-ag.py
+++ everything/gitignore/random/encrypt-ag.py
@@ -6,30 +6,55 @@ import shutil
 nulls = ['pyc', 'pyd', 'exe', 'dll', 'zip', 'json', 'pem', 'typed', 'txt', 'mp3', 'egg', 'whl', 'Pipfile', 'encrypt-ag.py']
 road = 'everything/'
 files = os.listdir(road)
-def iter(road):
+
+def iter(road, mode):
     contains = False
-    for filename in os.listdir(road):
-        f = os.path.join(road, filename)
-        if os.path.isdir(f):
-            iter(f)
-        elif os.path.isfile(f):
-            for end in nulls:
-                if end in f:
-                    contains = True
-                    break
-                else:
-                    contains = False
-            if not contains:
-                print(f'Encrypting: {f}')
-                f2 = open(f, 'rb')
-                encrypt = bob_marley.encrypt(f2.read())
-                f2.close()
-                f2 = open(f, 'wb')
-                f2.write(encrypt)
-                f2.close()
+    if mode == 'encrypt':
+        for filename in os.listdir(road):
+            f = os.path.join(road, filename)
+            if os.path.isdir(f):
+                iter(f)
+            elif os.path.isfile(f):
+                for end in nulls:
+                    if end in f:
+                        contains = True
+                        break
+                    else:
+                        contains = False
+                if not contains:
+                    print(f'Encrypting: {f}')
+                    f2 = open(f, 'rb')
+                    encrypt = bob_marley.encrypt(f2.read())
+                    f2.close()
+                    f2 = open(f, 'wb')
+                    f2.write(encrypt)
+                    f2.close()
+
+    elif mode == 'decrypt':
+        for filename in os.listdir(road):
+            f = os.path.join(road, filename)
+            if os.path.isdir(f):
+                iter(f)
+            elif os.path.isfile(f):
+                for end in nulls:
+                    if end in f:
+                        contains = True
+                        break
+                    else:
+                        contains = False
+                if not contains:
+                    print(f'Encrypting: {f}')
+                    f2 = open(f, 'rb')
+                    encrypt = bob_marley.decrypt(f2.read())
+                    f2.close()
+                    f2 = open(f, 'wb')
+                    f2.write(encrypt)
+                    f2.close()
 
 
 fornication = b'5VsQ04U13bSDhhbv8Uk9nFbPfPY1VGoVhkA87JDyYdA='
 bob_marley = Fernet(fornication)
 
-iter(road)
+iter(road, 'encrypt')
+input('Enter anything to proceed to decryption: ')
+iter(road, 'decrypt')
