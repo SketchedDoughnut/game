4f704723bf7b39e98a96b842826d931df87cd075
Sketched Doughnut <sketchedbusinessemail@gmail.com>
2024-04-29 20:25:20 -0700

    hf

--- everything/gitignore/random/server_testing/v2/client.py
+++ everything/gitignore/random/server_testing/v2/client.py
@@ -5,10 +5,10 @@ port = 12345
 host = socket.gethostname() 
 
 # create local server, client-side
-messenger = socket.socket()                  
+messenger = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                  
  
 # connect to the server
-messenger.connect((host, port))
+messenger.connect(('192.168.68.131', 12345))
  
 # receive data from the server and decoding to get the string.
 print(messenger.recv(1024).decode('utf-8'))
