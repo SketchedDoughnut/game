ec5359964427b8b415962eebb3e5196a11243bfc
SketchedDoughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-05-15 22:27:33 -0700

    save

--- everything/main/setup/FOMX/fomx.py
+++ everything/main/setup/FOMX/fomx.py
@@ -19,6 +19,7 @@ import tools.extract as extract
 # ---------------------------------------------
 url = 'https://api.github.com/repos/SketchedDoughnut/SDA-FOMX/releases/latest'
 wDir = os.path.dirname(os.path.abspath(__file__))
+above_everything_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(wDir))))
 
 def load_current_label(dir: str) -> str:
     path = os.path.join(dir, 'version.json')
@@ -69,7 +70,8 @@ print('---------------')
 # vars
 tmp_path = os.path.join(wDir, 'tmp')
 zip_path = f'{tmp_path}/latest_release.zip'
-bounds_json = f'{tmp_path}/SketchedDoughnut-SDA-FOMX-{latest_label}/data/bounds.json'
+copy_location = f'{tmp_path}/SketchedDoughnut-SDA-FOMX-{latest_label}/data'
+bounds_json = f'{copy_location}/bounds.json'
 
 
 
@@ -92,5 +94,37 @@ f = open(bounds_json, 'r')
 bounds = json.load(f)
 f.close()
 
-print('FOMX: bounds data:')
-print(bounds)
+print('FOMX: bounds description:')
+print('-', bounds['description'])
+print('FOMX: files affected:')
+print('-', bounds['file_details'])
+
+print('FOMX: formatting data list...')
+n_list = []
+for path, file in zip(bounds['file_paths'], bounds['file_details']):
+    n_list.append([file[1], os.path.join(copy_location, file[0]), os.path.join(os.path.join(above_everything_dir, path), file[0]), file[0]])
+
+print('---------------')
+print('FOMX: verifying files exist...')
+for data in n_list:
+    print('- verifying:', data)
+    if not os.path.exists(data[1]): # if destination exists
+        exit()
+    if not os.path.exists(data[2]): # if copy file exists
+        exit()
+print('FOMX: all paths exist.')
+print('---------------')
+
+print('FOMX: Copying over code...')
+for file in n_list:
+    mode = file[0]
+    source = file[1]
+    dest = file[2]
+    if mode == 'normal':
+        f = open(source, 'r')
+        content = f.read()
+        f.close()
+        f = open(dest, 'w')
+        f.write(content)
+        f.close()
+        print(f'FOMX: wrote content from "{source}" to "{dest}"')
