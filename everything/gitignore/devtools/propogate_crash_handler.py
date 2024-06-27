c0ffc4c987d887dda9b895c9784ae581b785d20e
SketchedDoughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-06-27 14:09:56 -0700

    saving propogator

--- everything/gitignore/devtools/propogate_crash_handler.py
+++ everything/gitignore/devtools/propogate_crash_handler.py
@@ -10,3 +10,22 @@ compiled_crash_file_list = [
     r'everything\main\setup\fiesta-modern.py',
     r'everything\main\setup\fiesta.py'
 ]
+
+crash_source = r'everything\gitignore\dev\crash_handler.py'
+
+f = open(crash_source, 'r')
+crash_source_content = f.read()
+f.close()
+print('Getting contents of source...')
+
+for file in raw_crash_file_list:
+    f = open(file, 'w')
+    f.write(crash_source_content)
+    f.close()
+    print('Propogating to raw file:', file)
+
+for file in compiled_crash_file_list:
+    f = open(file, 'w')
+    f.writable(crash_source_content)
+    f.close()
+    print('Propogating to compiled file:', file)
