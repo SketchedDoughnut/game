3f699ee69bc2fdd095b86edb84b4d543229bb41b
SketchedDoughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-06-27 13:59:31 -0700

    saving

--- everything/gitignore/devtools/propogate_crash_handler.py
+++ everything/gitignore/devtools/propogate_crash_handler.py
@@ -1,1 +1,12 @@
 # this file propogates the one template copy of the crash handler to every other crash handler
+raw_crash_file_list = [
+    r'everything\main\top\container\game_data\src\rhythm\rhythm.py',
+    r'everything\main\top\container\game_data\src\flappy\flappy.py',
+    r'everything\main\top\container\game_data\src\conways-game\main.py',
+    r'everything\main\top\container\index.py'
+]
+
+compiled_crash_file_list = [
+    r'everything\main\setup\fiesta-modern.py',
+    r'everything\main\setup\fiesta.py'
+]
