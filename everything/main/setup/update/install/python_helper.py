f85b371250a278cc977a4e01501e4acd93ee248b
SketchedDoughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-06-28 20:11:26 -0700

    working downloader from python

--- everything/main/setup/update/install/python_helper.py
+++ everything/main/setup/update/install/python_helper.py
@@ -90,13 +90,13 @@ class Python_helper:
     # CUSTOM FUNCTIONS
     def download(self):
         installer_data = requests.get('https://www.python.org/ftp/python/3.11.9/python-3.11.9-amd64.exe')
-        if installer_data.status_code == '200':
+        if installer_data.status_code == 200:
             tmp_path = self.wDir + '/tmp'
             try:
                 os.mkdir(tmp_path)
             except:
                 pass
-            exec_path = tmp_path + 'python-3.11.9-amd64.exe'
+            exec_path = tmp_path + '/python-3.11.9-amd64.exe'
             f = open(exec_path, 'wb')
             f.write(installer_data.content)
             f.close()
