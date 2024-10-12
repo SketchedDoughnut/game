427227197a10168c6cb0b0685357a9be0937a22a
Sketched Doughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-10-11 22:19:11 -0700

    quick fixes

--- build.py
+++ build.py
@@ -14,11 +14,13 @@ WDIR = os.path.dirname(os.path.abspath(__file__))
 
 # this is a function responsible for running the file lsit
 def do_file_list():
-    subprocess.call(os.path.join(WDIR, '/everything/toolsource/devtools/file_list.py'))
+    callpath = WDIR + '/everything/toolsource/devtools/file_list.py'
+    subprocess.call(callpath)
 
 # this is a function responsible for running the propagation
 def do_propagate(): 
-    subprocess.call(os.path.join(WDIR, '/everything/toolsource/devtools/propagator.py'))
+    callpath = WDIR + '/everything/toolsource/devtools/propagator.py'
+    subprocess.call(callpath)
 
 
 do_file_list()
